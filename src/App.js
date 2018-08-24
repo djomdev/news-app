eb24308fd86b564fb2b57e1ee9d77f7de75c615d
Diego Ortega <diegodeortega@MacBook-Pro-de-Diego.local>
2018-08-23 20:52:22 -0500

    Core React Fundamentals-Object key and import export

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import list from './list';
 
 //let's create some sample list of data
 
 
-const list = [
-  {
-    title: 'Tell me the truth witout words',
-    url: 'http://google.com',
-    author: 'Diego',
-    num_comments: 100,
-    points: 50,
-    objectID: 1
-  },
-  {
-    title: 'Lorem Ipsum',
-    url: 'http://google.com',
-    author: 'Jesus',
-    num_comments: 50,
-    points: 20,
-    objectID: 2
-  },
-  {
-    title: 'Columbae sont timidae',
-    url: 'http://google.com',
-    author: 'Ortega',
-    num_comments: 10,
-    points: 5,
-    objectID: 3
-  },
-]
+
 
 class App extends Component {
   render() {
@@ -40,7 +16,7 @@ class App extends Component {
         {
           list.map(function(item){
             return (
-            <div>
+            <div key={ item.objectID }>
                 <h1> <a href={ item.url }> {item.title} </a> by {item.author} </h1>
                 <h4>{ item.num_comments } Comments | { item.points } Points</h4>
             </div>
---
+++ src/list.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+const list = [
+    {
+        title: 'Tell me the truth witout words',
+        url: 'http://google.com',
+        author: 'Diego',
+        num_comments: 100,
+        points: 50,
+        objectID: 1
+    },
+    {
+        title: 'Lorem Ipsum',
+        url: 'http://google.com',
+        author: 'Jesus',
+        num_comments: 50,
+        points: 20,
+        objectID: 2
+    },
+    {
+        title: 'Columbae sont timidae',
+        url: 'http://google.com',
+        author: 'Ortega',
+        num_comments: 10,
+        points: 5,
+        objectID: 3
+    },
+]
+
+export default list;
