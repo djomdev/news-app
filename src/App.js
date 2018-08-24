f818de48530e9dc50f30583e30ccf419b4b22c41
Diego Ortega <diegodeortega@MacBook-Pro-de-Diego.local>
2018-08-23 21:08:42 -0500

    Setting up state local with constructor

--- src/App.js
+++ src/App.js
@@ -3,30 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 import list from './list';
 
-// class Developer {
-//   constructor(fname, lname) {
-//     this.fname = fname;
-//     this.lname = lname;
-//   }
-
-//   getName() {
-//     return this.fname + ' ' + this.lname;
-//   }
-// }
-
-// const diego = new Developer('Diego', 'Ortega');
-// console.log(diego.getName());
-
-// output: Diego Ortega
-
 
 class App extends Component {
+
+  // setting up internal component state
+  // ES6 class can use constructor to initialize state
+  constructor(props){
+    //super props sets this.props to the constructor
+    super(props);
+
+    //setting state
+    this.state = {
+      list: list
+    }
+
+  }
+
   render() {
     return (
       <div className="App">
 
         {
-          list.map( item =>
+          this.state.list.map( item =>
             <div key={ item.objectID }>
                 <h1> <a href={ item.url }> {item.title} </a> by {item.author} </h1>
                 <h4>{ item.num_comments } Comments | { item.points } Points</h4>
